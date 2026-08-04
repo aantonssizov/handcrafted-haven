e9a4a313bb9e7322b209d3635d37fd5b8ddb0259
Anton Syzov <aantonssizov@gmail.com>
2026-08-04 20:53:55 +0200

    fix images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import { heading } from "./fonts";
-import heroImage from "./assets/images/handcrafted-haven-hero.jpg";
-import artisanImage from "./assets/images/handcrafted-haven-artisans.jpg";
-import customerImage from "./assets/images/handcrafted-haven-costomers.jpg";
+import heroImage from "../../public/images/handcrafted-haven-hero.jpg";
+import artisanImage from "../../public/images/handcrafted-haven-artisans.jpg";
+import customerImage from "../../public/images/handcrafted-haven-costomers.jpg";
 
 export default function Home() {
   return (
